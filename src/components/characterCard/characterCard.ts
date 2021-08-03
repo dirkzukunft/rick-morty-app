02d59ffadd661bda0ee52d04de678670151f5343
dirkzukunft <git@qf3.de>
2021-08-03 16:47:55 +0200

    Add character card details

--- src/components/characterCard/characterCard.ts
+++ src/components/characterCard/characterCard.ts
@@ -1,36 +1,27 @@
 import { createElement } from '../../../utils/createElement';
 import './characterCard.css';
 
-export function characterCard() {
-  const character = {
-    id: 235,
-    name: 'Mortytown Loco',
-    status: 'Dead',
-    species: 'Human',
-    type: '',
-    gender: 'Male',
-    origin: {
-      name: 'unknown',
-      url: '',
-    },
-    location: {
-      name: 'Citadel of Ricks',
-      url: 'https://rickandmortyapi.com/api/location/3',
-    },
-    image: 'https://rickandmortyapi.com/api/character/avatar/235.jpeg',
-    episode: ['https://rickandmortyapi.com/api/episode/28'],
-    url: 'https://rickandmortyapi.com/api/character/235',
-    created: '2017-12-30T16:37:07.150Z',
-  };
-
+export function characterCard(character) {
   return createElement('article', {
     className: 'characterCard',
     childElements: [
       createElement('img', { src: character.image }),
       createElement('div', {
         childElements: [
-          createElement('p', { innerText: 'test' }),
-          createElement('p', { innerText: 'test' }),
+          createElement('h3', { innerText: character.name }),
+          createElement('p', {
+            innerText: `${character.status} - ${character.species}`,
+          }),
+          createElement('p', {
+            innerText: 'Last known location:',
+            className: 'caption',
+          }),
+          createElement('p', { innerText: character.location.name }),
+          createElement('p', {
+            innerText: 'First seen in:',
+            className: 'caption',
+          }),
+          createElement('p', { innerText: character.origin.name }),
         ],
       }),
     ],
