1ac47c6a4ba48bc719cce49719f2bbb47f67bafa
dirkzukunft <git@qf3.de>
2021-08-03 14:35:49 +0200

    Refactoring using createElement function

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
+import { createElement } from '../utils/createElement';
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const main = document.createElement('main');
-
-const headline = document.createElement('h1');
-headline.innerHTML = `Rick <span>and</span> Morty`;
-headline.className = 'headline';
-
-const input = document.createElement('input');
-input.className = 'input';
-input.placeholder = 'Search for a character...';
-
-main.append(headline, input);
+const main = createElement('main', {
+  childElements: [
+    createElement('h1', {
+      innerHTML: 'Rick <span>and</span> Morty',
+      className: 'headline',
+    }),
+    createElement('input', {
+      className: 'input',
+      placeholder: 'Search for a character...',
+    }),
+  ],
+});
 
 if (app !== null) {
   app.append(main);
