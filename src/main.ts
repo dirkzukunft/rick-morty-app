ba12f8246dd9434b2e51c6b9df6eb057c16afedc
dirkzukunft <git@qf3.de>
2021-08-05 15:39:19 +0200

    Mapping characters and show them on the website

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,14 @@
 import { createElement } from '../utils/createElement';
 import { getCharacters } from '../utils/api';
 import { createCharacterCard } from './components/characterCard/characterCard';
-import { Character } from './types';
 import './style.css';
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
-const characters: Character[] = [
-  {
-    name: 'Beebo',
-    status: 'Dead',
-    species: 'Alien',
-    origin: "Morty's Mind Blowers",
-    location: 'Venzenulon 7',
-    image: 'https://rickandmortyapi.com/api/character/avatar/33.jpeg',
-  },
-  {
-    name: 'Evil Beth Clone',
-    status: 'Dead',
-    species: 'Human',
-    origin: 'unknown',
-    location: 'Earth (C-137)',
-    image: 'https://rickandmortyapi.com/api/character/avatar/116.jpeg',
-  },
-  {
-    name: 'Rick Sanchez',
-    status: 'Dead',
-    species: 'Human',
-    origin: 'Earth (Replacement Dimension)',
-    location: 'Earth (Replacement Dimension)',
-    image: 'https://rickandmortyapi.com/api/character/avatar/293.jpeg',
-  },
-  {
-    name: 'Riq IV',
-    status: 'Dead',
-    species: 'Human',
-    origin: 'unknown',
-    location: 'Citadel of Ricks',
-    image: 'https://rickandmortyapi.com/api/character/avatar/295.jpeg',
-  },
-];
+// getCharacters()
+//   .then((data) => console.log(data))
+//   .catch(() => console.log('Error'));
+const characters = await getCharacters();
 
 const main = createElement('main', {
   childElements: [
@@ -58,11 +27,6 @@ const main = createElement('main', {
     }),
   ],
 });
-
-getCharacters()
-  .then((data) => console.log(data))
-  .catch(() => console.log('Error'));
-//console.log(getCharacters());
 
 if (app !== null) {
   app.append(main);
